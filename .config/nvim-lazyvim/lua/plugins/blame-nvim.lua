9da60ef93a82f8af760b53809e081cedd35e5cf1
resurtm <resurtm@gmail.com>
2024-03-02 21:52:24 +0100

    Neovim: Add enable/disable per-line Git blame.

--- .config/nvim-lazyvim/lua/plugins/blame-nvim.lua
+++ .config/nvim-lazyvim/lua/plugins/blame-nvim.lua
@@ -4,5 +4,9 @@ return {
     opts = {
       merge_consecutive = true,
     },
+    keys = {
+      { "<leader>gb", "<cmd>EnableBlame virtual<cr>", desc = "enable, blame for all LOCs" },
+      { "<leader>gB", "<cmd>DisableBlame<cr>", desc = "enable, blame for all LOCs" },
+    },
   },
 }
