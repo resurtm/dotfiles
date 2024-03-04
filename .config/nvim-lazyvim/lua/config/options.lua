750c922ae591aa0be6dd29826c2757e3a721abb9
resurtm <resurtm@gmail.com>
2024-03-04 17:35:38 +0100

    Update LazyVim plugins.

--- .config/nvim-lazyvim/lua/config/options.lua
+++ .config/nvim-lazyvim/lua/config/options.lua
@@ -2,9 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- code folding: https://www.jmaguire.tech/posts/treesitter_folding/
 local vim = vim
 local opt = vim.opt
+
+-- code folding: https://www.jmaguire.tech/posts/treesitter_folding/
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 
