05bf01428c708ecd052010bda760657ed7c3b0fa
resurtm <resurtm@gmail.com>
2024-03-07 15:30:05 +0100

    More additions to the self-built Neovim config.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "lua", "javascript" },
+      ensure_installed = { "lua", "javascript", "markdown", "markdown_inline" },
       highlight = { enable = true },
       indent = { enable = true },
     })
