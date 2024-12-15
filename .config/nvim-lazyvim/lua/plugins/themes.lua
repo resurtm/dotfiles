8ad4439ff84d663da62c3fe0dd8e6faa991af943
resurtm <resurtm@gmail.com>
2024-12-15 17:02:56 +0100

    LazyVim extras + Alacritty font

--- .config/nvim-lazyvim/lua/plugins/themes.lua
+++ .config/nvim-lazyvim/lua/plugins/themes.lua
@@ -1,9 +1,20 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
+  { "nyoom-engineering/oxocarbon.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox",
+      -- colorscheme = "oxocarbon",
     },
+  },
+  {
+    "lukas-reineke/virt-column.nvim",
+    config = function()
+      require("virt-column").setup({
+        char = "▕",
+        virtcolumn = "80,120",
+      })
+    end,
   },
 }
