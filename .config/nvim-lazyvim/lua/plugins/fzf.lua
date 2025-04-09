45c4690aa0448b2013439aa48e1b22ffd2e9533a
resurtm <resurtm@gmail.com>
2025-04-09 14:07:52 +0200

    Adjust fzf-lua shortcuts

---
+++ .config/nvim-lazyvim/lua/plugins/fzf.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    cmd = "FzfLua",
+    opts = function(_, opts)
+      local actions = require("fzf-lua.actions")
+      return vim.tbl_deep_extend("force", opts, {
+        files = {
+          cwd_prompt = false,
+          actions = {
+            ["ctrl-i"] = { actions.toggle_ignore },
+            ["ctrl-h"] = { actions.toggle_hidden },
+          },
+        },
+        grep = {
+          actions = {
+            ["ctrl-i"] = { actions.toggle_ignore },
+            ["ctrl-h"] = { actions.toggle_hidden },
+          },
+        },
+      })
+    end,
+  },
+}
