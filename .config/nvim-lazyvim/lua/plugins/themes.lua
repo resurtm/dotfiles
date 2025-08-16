dc0679f4101a02e093dd20f473fd7551a37c287f
resurtm <resurtm@gmail.com>
2025-08-16 15:57:57 +0200

    Tweak cyberdream theme

--- .config/nvim-lazyvim/lua/plugins/themes.lua
+++ .config/nvim-lazyvim/lua/plugins/themes.lua
@@ -4,6 +4,22 @@ return {
     priority = 1000,
     opts = {
       cache = true,
+      colors = {
+        dark_gray = "#272a30",
+        mid_gray = "#5f5f5f",
+      },
+      overrides = function(t)
+        return {
+          -- https://github.com/scottmckendry/cyberdream.nvim/blob/main/lua/cyberdream/colors.lua#L4
+          -- https://github.com/scottmckendry/cyberdream.nvim/blob/main/lua/cyberdream/extensions/base.lua#L43-L45
+          -- NormalFloat = { fg = t.fg, bg = t.bg_highlight },
+          -- FloatTitle = { fg = t.cyan, bg = t.bg_highlight },
+          -- FloatBorder = { fg = t.bg, bg = t.bg_highlight },
+          NormalFloat = { fg = t.fg, bg = t.dark_gray },
+          FloatTitle = { fg = t.cyan, bg = t.dark_gray },
+          FloatBorder = { fg = t.mid_gray, bg = t.dark_gray },
+        }
+      end,
     },
   },
   {
@@ -20,11 +36,20 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
+      -- colorscheme = "miasma",
       colorscheme = "cyberdream",
       -- colorscheme = "oxocarbon",
       -- colorscheme = "modus",
       -- colorscheme = "gruvbox",
     },
+  },
+  {
+    "xero/miasma.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- vim.cmd("colorscheme miasma")
+    end,
   },
   -- {
   --   "lukas-reineke/virt-column.nvim",
