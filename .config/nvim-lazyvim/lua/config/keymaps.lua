43f49ad31b42e71ba8715f5bed0bc4266f5eddb7
resurtm <resurtm@gmail.com>
2026-01-15 13:11:59 +0100

    Disable arrow keys in NeoVim, use HJKL always

--- .config/nvim-lazyvim/lua/config/keymaps.lua
+++ .config/nvim-lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,27 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Disable arrow keys in Normal mode
+vim.keymap.set("n", "<Down>", "<Nop>", { noremap = true })
+vim.keymap.set("n", "<Left>", "<Nop>", { noremap = true })
+vim.keymap.set("n", "<Right>", "<Nop>", { noremap = true })
+vim.keymap.set("n", "<Up>", "<Nop>", { noremap = true })
+
+-- Disable arrow keys in Insert mode
+vim.keymap.set("i", "<Down>", "<Nop>", { noremap = true })
+vim.keymap.set("i", "<Left>", "<Nop>", { noremap = true })
+vim.keymap.set("i", "<Right>", "<Nop>", { noremap = true })
+vim.keymap.set("i", "<Up>", "<Nop>", { noremap = true })
+
+-- Disable arrow keys in Command mode
+vim.keymap.set("c", "<Down>", "<Nop>", { noremap = true })
+vim.keymap.set("c", "<Left>", "<Nop>", { noremap = true })
+vim.keymap.set("c", "<Right>", "<Nop>", { noremap = true })
+vim.keymap.set("c", "<Up>", "<Nop>", { noremap = true })
+
+-- Disable arrow keys in Visual mode
+vim.keymap.set("v", "<Down>", "<Nop>", { noremap = true })
+vim.keymap.set("v", "<Left>", "<Nop>", { noremap = true })
+vim.keymap.set("v", "<Right>", "<Nop>", { noremap = true })
+vim.keymap.set("v", "<Up>", "<Nop>", { noremap = true })
