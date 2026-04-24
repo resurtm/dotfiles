5e9f3760927e942d9ed64ba0880a19643a993b1c
resurtm <resurtm@gmail.com>
2026-04-24 09:06:02 +0200

    Void, dump groups and runit services

---
+++ .etc/void/etc.md
@@ -0,0 +1,33 @@
+# Other things
+
+## Groups
+
+```
+resurtm : resurtm wheel lp audio video storage scanner network kvm input plugdev users xbuilder dbus socklog
+```
+
+## Runit
+
+```
+agetty-tty1
+agetty-tty2
+agetty-tty3
+agetty-tty4
+agetty-tty5
+agetty-tty6
+bluetoothd
+chronyd
+cupsd
+dbus
+dhcpcd
+elogind
+fcron
+lact
+nanoklogd
+polkitd
+power-profiles-daemon
+saned
+socklog-unix
+udevd
+wireguard
+```
--- .etc/void/update.sh
+++ .etc/void/update.sh
@@ -3,8 +3,9 @@
 dir="${HOME}/.etc/void"
 ffile="${dir}/flatpak.md"
 xfile="${dir}/xbps.md"
+efile="${dir}/etc.md"
 
-rm -f -- "$ffile" "$xfile"
+rm -f -- "$ffile" "$xfile" "$efile"
 mkdir -p -- "$dir"
 
 echo "# Void Flatpak\n\n## List\n\n\`\`\`" >>"$ffile"
@@ -16,3 +17,9 @@ xpkg -m | sort >>"$xfile"
 echo "\`\`\`\n\n## Ignored\n\n\`\`\`" >>"$xfile"
 cat /etc/xbps.d/ignorepkg.conf | sort >>"$xfile"
 echo "\`\`\`" >>"$xfile"
+
+echo "# Other things\n\n## Groups\n\n\`\`\`" >>"$efile"
+groups resurtm >>"$efile"
+echo "\`\`\`\n\n## Runit\n\n\`\`\`" >>"$efile"
+ls /var/service/ | sort >>"$efile"
+echo "\`\`\`" >>"$efile"
